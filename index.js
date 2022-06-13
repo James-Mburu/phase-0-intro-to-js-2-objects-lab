f63c8617c5ef36564f59d21931dfa546ee641d17
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-13 13:50:28 +0300

    commit tree ec7e20ab11eb19a0065af113f8bd0078651dfa1f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+
+    const employee = {
+        name: "Stella",
+        streetAddress: "Ngeka Centre"
+
+    }
+
+  function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = { ...employee };
+
+    newEmployee[key] = value;
+
+    return newEmployee
+
+    }
+
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee[key] = value;
+        return employee
+    }
+
+
+     function deleteFromEmployeeByKey(employee, key){     
+    //    deleteFromEmployeeByKey(employee, key)
+        let newEmployee = { ...employee };
+         delete newEmployee['name']
+
+         return newEmployee
+
+     }
+
+     function destructivelyDeleteFromEmployeeByKey(employee, key){
+        delete employee['name']
+
+         return employee
+     }
